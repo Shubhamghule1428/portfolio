873588bd937284ed9117448c8207dbaaef1e62b9
Shubham Ghule <wweshivamghule149@gmail.com>
2022-09-17 19:11:31 +0530

    final commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,51 @@
 import React from "react";
 import "./nav.css";
-import {GoHome} from "react-icons/go"
-import {ImUser} from "react-icons/im"
-import {AiOutlineFundProjectionScreen} from "react-icons/ai"
-import {GiBookshelf} from "react-icons/gi"
-import {AiOutlineMessage} from "react-icons/ai"
+import { GoHome } from "react-icons/go";
+import { ImUser } from "react-icons/im";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { GiBookshelf } from "react-icons/gi";
+import { AiOutlineMessage } from "react-icons/ai";
 import { useState } from "react";
 
 const Nav = () => {
-  const [activeNav, setActiveNav]= useState('#')
-  return(
+  const [activeNav, setActiveNav] = useState("#");
+  return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><GoHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><ImUser/></a>
-      <a href="#experince" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><GiBookshelf/></a>
-      <a href="#project" onClick={() => setActiveNav('#project')} className={activeNav === '#project' ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <GoHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <ImUser />
+      </a>
+      <a
+        href="#experince"
+        onClick={() => setActiveNav("#experince")}
+        className={activeNav === "#experince" ? "active" : ""}
+      >
+        <GiBookshelf />
+      </a>
+      <a
+        href="#project"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <AiOutlineFundProjectionScreen />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMessage />
+      </a>
     </nav>
   );
 };
